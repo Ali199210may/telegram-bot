6eb5ce37d420d2b6ea9dd05047b7166ec40b178a
Ali199210may <dispatch777logistic@gmail.com>
2026-05-14 03:31:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -775,7 +775,21 @@ if __name__ == '__main__':
 
     print("BOT ISHLADI")
 
+    from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot ishlayapti"
+
+def run_web():
+    app.run(host="0.0.0.0", port=10000)
+
+Thread(target=run_web).start()
     bot.infinity_polling()
+
 
 # ===== TAVARNI TAHRIRLASH =====
 
